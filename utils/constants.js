8461cf3d4b20ebf4a5fc86a6753364fe080ccde6
Anastasia Semenova <n4stenkas91@yandex.ru>
2023-02-19 14:45:37 +0300

    adaptivity

---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+export const sumInputSelector = '.sum-input';
+export const personsInputSelector = '.persons-input';
+export const discountSelector = '.discount';
+export const customInputSelector = '.custom-input';
+export const tipSelector = '.tip';
+export const totalSelector = '.total';
+export const resetButtonSelector = '.reset';
